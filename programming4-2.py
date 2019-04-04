1fe4de21a5f6454ac90294ee6465be9f1571edb5
kimminhyeok13 <kimminhyeok13@gmail.com>
2019-04-04 22:13:34 +0900

    final

--- programming4-2.py
+++ programming4-2.py
@@ -2,32 +2,52 @@
 from operator import itemgetter
 
 
+def month_to_int(month):
+	dic = {'Jan' : '1', 'Feb' : '2', 'Mar':'3', 'Apr': '4', 'May' : '5', 'Jun':'6', 'Jul':'7',
+		'Aug':'8','Sep':'9','Oct':'10','Nov':'11','Dec':'12'}
+
+	return dic[month]
+
+def int_to_month(month):
+	dic = {'1' : 'Jan', '2' : 'Feb', '3':'Mar', '4': 'Apr', '5' : 'May', '6':'Jun', '7':'Jul',
+		'8':'Aug','9':'Sep','10':'Oct','11':'Nov','12':'Dec'}
+	return dic[month]
+
+
 def read_weblog(filename):
-	fp=open(filename,"r")
+	fp = open(filename, "r")
 	fp.readline()
-	index=0
+	index = 0
 	while True:
-		tmp_lis=[]
-		one_line=fp.readline().split("\n")[0]
+		tmp_lis = []
+		one_line = fp.readline().split("\n")[0]
 		if not one_line:
 			break
-		ip,time,url,status=one_line.split(",")
-		log_info=[ip,time,url,status]
+		ip, time, url, status = one_line.split(",")
+		time = time.split('[')[1]
+		time = time.split('/')
+		day, month = time[0],month_to_int(time[1])
+		time = time[2].split(':')
+		year, hour, minute, seconds = time[0],time[1],time[2],time[3]
+		log_info = [ip, day,month,year,hour,minute,seconds, url, status]
 		weblog_list.append(log_info)
 		index+=1
 	fp.close()
+	
 
-
-
-weblog_list=[]
+# time = day, month, year, hour,min,seconds
+# ip = A,B,C,D
+weblog_list = []
 
 def main():
+	sort_flag = 0
 	while True :
 		string = input("$ ")
-		command_lis=string.split()
-		command1=command_lis[0]
+		command_lis = string.split()
+		command1 = command_lis[0]
 
-
+def  adfafsf(elem):
+	return elem[0]
 
 		if command1 == "exit":
 			break
@@ -35,25 +55,37 @@ def main():
 		if command1 == "sort" and len(command_lis)==2:
 			command2=command_lis[1]
 			if command2 == "-ip":
-				weblog_list.sort(key=itemgetter(0))
+				
+				weblog_list.sort(key = lambda elem: elem[0])
+				#print(weblog_list[0],weblog_list[1],weblog_list[2],weblog_list[3])
+				sort_flag='ip'
+			
 			elif command2 == "-t":
-				weblog_list.sort(key=itemgetter(1))
-			elif command2 == "-url":
-				weblog_list.sort(key=itemgetter(2))
-			elif command2 == "-status":
-				weblog_list.sort(key=itemgetter(3))
+				
+				for i in [6 ,5 ,4 ,1 ,2 , 3]:
+					weblog_list.sort(key= lambda elem: elem[i])
+				sort_flag='time'
 		if command1 == "read" and len(command_lis)==2:
 			command2=command_lis[1]
 			read_weblog(command2)
 
 		if command1 == "print":
-			for content in weblog_list:
-				print(content[0])
-				print(content[1])
-				print(content[2])
-				print(content[3])
-				print("\n-------------------\n")
-
+			if sort_flag == 0:
+				print("No Sort...\n")
+			if sort_flag == 'ip':
+				for content in weblog_list:
+					print(content[0])
+					print('\t'+content[1]+'/'+int_to_month(content[2])+'/'+content[3]+':'+content[4]+':'+content[5]+':'+content[6])
+					print('\t'+content[7])
+					print('\t'+content[8])
+					print("\n-------------------\n")
+			if sort_flag == 'time':
+				for content in weblog_list:
+					print(content[1]+'/'+int_to_month(content[2])+'/'+content[3]+':'+content[4]+':'+content[5]+':'+content[6])
+					print('\t'+content[0])
+					print('\t'+content[7])
+					print('\t'+content[8])
+					print("\n-------------------\n")
 
 main()
 
